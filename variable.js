1f844d55d3c46dadd52616287173b14575fe1cb8
devlrakib <devrakib5@gmail.com>
2024-03-05 14:31:23 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,12 @@
+// *String Type Variable;
+var person = "Sadar Uddin";
+var location = "Dhanmandi, Dhaka";
+
+// ?Number Type Variable;
+var age = "25";
+var Roll = "203519";
+
+// Boolean Data Type Variable;
+var serious ="true";
+var isFullmarks ="true";
+var isSingle = "false";
